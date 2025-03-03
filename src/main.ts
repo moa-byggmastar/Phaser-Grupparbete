ded0e49a8293c4dd32a354f7f76db51bf33d6b56
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-03-03 13:26:52 +0200

    chore: replace Vite default files with Phaser setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import Phaser from 'phaser';
+import GameScene from './scenes/gamescene';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 450,
+    parent: 'app',
+    scene: [GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 800 },
+            debug: false
+        }
+    },
+    antialias: false
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const game = new Phaser.Game(config);
---
+++ src/scenes/gamescene.ts
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('gamescene');
+    };
+
+    preload() {
+
+    };
+
+    create() {
+    };
+
+    update(time: number, delta: number): void {
+    };
+
+};
