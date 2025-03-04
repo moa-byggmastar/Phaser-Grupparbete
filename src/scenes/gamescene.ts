11bb87c96e6a3685735d47b823981d6394c9525d
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-03-04 14:56:40 +0200

    Added testing sprite to player

--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import Text from "../graphics/text";
+import Player from "../entities/player";
 
 export default class GameScene extends Phaser.Scene {
     constructor() {
@@ -7,11 +8,17 @@ export default class GameScene extends Phaser.Scene {
     };
 
     preload() {
-
+        this.load.image('player', 'src/assets/pyre-maid.png')
     };
 
     create() {
-        new Text(this, Number(this.game.config.width) / 2, 100, 'Game scene', 60) // Temporary, remove if needed
+
+         // Temporary, remove if needed
+        new Text(this, Number(this.game.config.width) / 2, 100, 'Game scene', 60)
+        let player = new Player(this, Number(this.game.config.width) / 2, Number(this.game.config.width) / 2)
+        player.setTexture('player')
+        // ---
+        
     };
 
     update(time: number, delta: number): void {
