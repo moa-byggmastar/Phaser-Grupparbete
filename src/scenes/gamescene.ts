3d7a88f44ca89aad5455dac91cb4c95d753576eb
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-03-10 10:14:14 +0200

    multiple enemies test

--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -5,8 +5,8 @@ import Enemy from "../entities/enemy";
 
 export default class GameScene extends Phaser.Scene {
     player!: Player;
-    enemy!: Enemy;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+    enemies: any;
     constructor() {
         super('gamescene');
     };
@@ -21,14 +21,24 @@ export default class GameScene extends Phaser.Scene {
         // Temporary, remove if needed
         new Text(this, Number(this.game.config.width) / 2, 100, 'Game scene', 60)
         this.player = new Player(this, Number(this.game.config.width) / 2, Number(this.game.config.width) / 2)
-        this.enemy = new Enemy(this, 800, 450, 'enemy1')
+        this.enemies = []
+        this.enemies.push(new Enemy(this, 800, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 700, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 600, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 500, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 300, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 200, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 100, 450, 'enemy1'))
+        this.enemies.push(new Enemy(this, 0, 450, 'enemy1'))
         this.cursors = this.input.keyboard!.createCursorKeys()
         // ---
 
     };
 
     update(time: number, delta: number): void {
-        this.enemy.update()
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].update()
+        }
         this.player.update(time, this.cursors!)
     };
 
