5d356c1c2a6c24deed96fc01812f58b181b85e75
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-03-10 09:59:35 +0200

    Added basic enemy functionality

--- src/entities/enemy.ts
+++ src/entities/enemy.ts
@@ -1,5 +1,47 @@
-import Phaser from 'phaser'
+import { Physics, Scene } from 'phaser'
 
-export default class Enemy {
+export default class Enemy extends Physics.Arcade.Sprite {
+    private speed = 25
+    private health = 100
+    private targetX: number
+    private targetY: number
+    declare body: Phaser.Physics.Arcade.Body
 
+    constructor(scene: Scene, x: number, y: number) {
+        super(scene, x, y, 'enemy1')
+
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+
+        // Change to fire's position when possible
+        this.targetX = scene.scale.width / 2
+        this.targetY = scene.scale.height / 2
+
+        this.body!.setAllowGravity(false)
+    }
+
+    public update() {
+        // Calculate angle towards the target
+        const angle = Phaser.Math.Angle.Between(this.x, this.y, this.targetX, this.targetY)
+        
+        // Set velocity in that direction
+        this.setVelocity(Math.cos(angle) * this.speed, Math.sin(angle) * this.speed)
+
+        // Flips sprite to match direction
+        this.flipX = Math.cos(angle) < 0;
+    }
+
+    public takeDamage(amount: number) {
+        this.health -= amount
+        if (this.health <= 0) {
+            this.kill()
+        }
+    }
+
+    private kill() {
+        console.log('Enemy killed')
+        this.setActive(false)
+        this.setVisible(false)
+        this.body.setVelocity(0, 0)
+    }
 }
--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -1,28 +1,34 @@
 import Phaser from "phaser";
 import Text from "../graphics/text";
 import Player from "../entities/player";
+import Enemy from "../entities/enemy";
 
 export default class GameScene extends Phaser.Scene {
+    player!: Player;
+    enemy!: Enemy;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
     constructor() {
         super('gamescene');
     };
 
     preload() {
         this.load.image('player', 'src/assets/pyre-maid.png')
+        this.load.image('enemy1', 'src/assets/weakenemy.png')
     };
 
     create() {
 
          // Temporary, remove if needed
         new Text(this, Number(this.game.config.width) / 2, 100, 'Game scene', 60)
-        let player = new Player(this, Number(this.game.config.width) / 2, Number(this.game.config.width) / 2)
-        player.setTexture('player')
+
+        this.enemy = new Enemy(this, 800, 450)
+
         // ---
         
     };
 
     update(time: number, delta: number): void {
-
+        this.enemy.update()
     };
 
 };
