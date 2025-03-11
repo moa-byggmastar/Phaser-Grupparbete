4d89ef967b3cbc47d8c8e4eb4274ee28e7678760
moa-byggmastar <moabygg07@gmail.com>
2025-03-11 13:47:32 +0200

    game stops on collision with enemies

--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -21,6 +21,7 @@ export default class GameScene extends Phaser.Scene {
         // Temporary, remove if needed
         new Text(this, Number(this.game.config.width) / 2, 100, 'Game scene', 60)
         this.player = new Player(this, Number(this.game.config.width) / 2, Number(this.game.config.width) / 2)
+
         this.enemies = []
         this.enemies.push(new Enemy(this, 800, 450, 'enemy1'))
         this.enemies.push(new Enemy(this, 700, 450, 'enemy1'))
@@ -30,6 +31,9 @@ export default class GameScene extends Phaser.Scene {
         this.enemies.push(new Enemy(this, 200, 450, 'enemy1'))
         this.enemies.push(new Enemy(this, 100, 450, 'enemy1'))
         this.enemies.push(new Enemy(this, 0, 450, 'enemy1'))
+
+        this.physics.add.collider(this.player, this.enemies, this.handleCollision, undefined, this)
+
         this.cursors = this.input.keyboard!.createCursorKeys()
         // ---
 
@@ -42,4 +46,8 @@ export default class GameScene extends Phaser.Scene {
         this.player.update(time, this.cursors!)
     };
 
+    private handleCollision(player: Player, enemy: Enemy) {
+        this.scene.pause()
+        alert('Game Over!')
+    }
 };
